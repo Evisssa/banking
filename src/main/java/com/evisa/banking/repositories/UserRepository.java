d2ebf31a675168f83eaf7f3fa55ed70e26722b58
Evisa Skarra <evisa.skarra@gmail.com>
2024-06-22 23:59:41 +0200

    Adding UserRepository

---
+++ src/main/java/com/evisa/banking/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.evisa.banking.repositories;
+
+import com.evisa.banking.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
